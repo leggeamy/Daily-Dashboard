52918a4f8ca7c067c2ecba458b4f25b54c55a054
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2020-10-16 02:43:08 -0400

    completed first version of function to create news articles using thumbnail, headline and shurt url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,40 @@
-function getNews () {
-    var newsCategory = "crime";
-    var newsApiKey = "&api-key=2d7a1e64-833c-4dd6-b4cc-5d27dee745b0";
-    var fromDate = "from-date=2020-10-15"
-    fetch (
-        "https://content.guardianapis.com/search?q=" +
-        // get user selected news category
-        newsCategory+
-        "&format=json&" +
-        fromDate +
-        "&show-fields=headline,thumbnail,short-url&order-by-relevance" +
-        newsApiKey
-        ).then(function(response) {
-            console.log(response.json());
-        });
+
+var newsApiKey = "&api-key=2d7a1e64-833c-4dd6-b4cc-5d27dee745b0";
+
+
+function getNews() {
+    var newsCategory = "politics";
+    var fromDate = "from-date=2020-10-16"
+    console.log(newsCategory);
+    fetch(
+        `https://content.guardianapis.com/search?q=${newsCategory}&format=json&${fromDate}&show-fields=headline,thumbnail,short-url&order-by-relevance${newsApiKey}`
+    ).then(function (response) {
+        return response.json();
+    }).then(function (articles) {
+        console.log(articles);
+        for (i = 0; i < 4; i++) {
+            // create DOM Elements used to display news articles on page
+            var newsContainerEl = document.querySelector(".news-container");
+            var articleEl = document.createElement("div");
+            var articleLinkEl = document.createElement("a");
+            var articleImgEl = document.createElement("img");
+            var articleHeadlineEl = document.createElement("p");
+
+            // add content to DOM Elements
+            // articleLinkEl.innerHTML = 'href="' + articles.response.results[i].fields.shortUrl + '">' + '<img src="' + articles.response.results[i].fields.thumbnail + '">';
+            articleLinkEl.setAttribute("href",articles.response.results[i].fields.shortUrl);
+            articleLinkEl.setAttribute("target","_blank");
+            articleImgEl.setAttribute("src", articles.response.results[i].fields.thumbnail);
+            articleHeadlineEl.textContent = articles.response.results[i].fields.headline;
+
+            // combine DOM Elements and add  to container
+            articleLinkEl.append(articleImgEl);
+            articleEl.appendChild(articleLinkEl);
+            articleEl.appendChild(articleHeadlineEl);
+            newsContainerEl.appendChild(articleEl);
+
+        }
+    })
 };
 
 
