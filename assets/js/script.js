da628a51e0f5d3f32e5ee7bd58e2f683b4e04aca
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2020-10-15 23:41:42 -0400

    refined function getNews using query parameters from API Documentation - filter by keyword, from-date, sort by relevance and request specific content fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 function getNews () {
-    var newsCategory = "politics";
-    var newsApiKey = "api-key=2d7a1e64-833c-4dd6-b4cc-5d27dee745b0";
+    var newsCategory = "crime";
+    var newsApiKey = "&api-key=2d7a1e64-833c-4dd6-b4cc-5d27dee745b0";
+    var fromDate = "from-date=2020-10-15"
     fetch (
         "https://content.guardianapis.com/search?q=" +
         // get user selected news category
         newsCategory+
-        "&format=json&tag=us/politics,world/politics&from-date=2020-10-01&show-fields=headline,thumbnail,short-url&order-by-relevance" +
+        "&format=json&" +
+        fromDate +
+        "&show-fields=headline,thumbnail,short-url&order-by-relevance" +
         newsApiKey
         ).then(function(response) {
-            console.log(response)
+            console.log(response.json());
         });
 };
 
