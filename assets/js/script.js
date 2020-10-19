dfeaedad1913bed803137cde29ef82d65a8e405a
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2020-10-19 00:21:52 -0400

    added functionality to get user input for news fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,67 @@
-
+// Store API Key in a variable
 var newsApiKey = "&api-key=2d7a1e64-833c-4dd6-b4cc-5d27dee745b0";
 
+// Find user selected news category
+function getNewsCategory() {
+    var newsCategory = document.querySelector(".selected-news").textContent;
+    console.log(newsCategory);
+    getNews(newsCategory);
+}
 
 function getNews() {
-    var newsCategory = "politics";
-    var fromDate = "from-date=2020-10-16"
-    console.log(newsCategory);
+    // var newsCategory = "politics";
+    var fromDate = moment().format("YYYY-MM-DD");
+    console.log(fromDate);
     fetch(
-        `https://content.guardianapis.com/search?q=${newsCategory}&format=json&${fromDate}&show-fields=headline,thumbnail,short-url&order-by-relevance${newsApiKey}`
+        `https://content.guardianapis.com/search?q=${newsCategory}&format=json&${fromDate}&show-fields=headline,byline,trailText,thumbnail,short-url&order-by-relevance${newsApiKey}`
     ).then(function (response) {
         return response.json();
     }).then(function (articles) {
         console.log(articles);
         for (i = 0; i < 4; i++) {
-            // create DOM Elements used to display news articles on page
-            var newsContainerEl = document.querySelector(".news-container");
-            var articleEl = document.createElement("div");
-            var articleLinkEl = document.createElement("a");
+            if (i < 2) {
+                // create DOM Elements used to display news articles on page
+                var newsContainerEl = document.querySelector("#carousel-first-slide");
+                newsContainerEl.classList.add("carousel-item", "active");
+            }
+            else {
+                var newsContainerEl = document.querySelector("#carousel-second-slide");
+                newsContainerEl.classList.add("carousel-item");
+            }
+            var articleContainerEl = document.createElement("div");
+            var articleLabelEl = document.createElement("div");
+            var articleHeadlineEl = document.createElement("h3");
+            var articleAuthorEl = document.createElement("h4");
             var articleImgEl = document.createElement("img");
-            var articleHeadlineEl = document.createElement("p");
+            var articleSummaryEl = document.createElement("p");
+            var articleLinkEl = document.createElement("a");
 
             // add content to DOM Elements
-            // articleLinkEl.innerHTML = 'href="' + articles.response.results[i].fields.shortUrl + '">' + '<img src="' + articles.response.results[i].fields.thumbnail + '">';
-            articleLinkEl.setAttribute("href",articles.response.results[i].fields.shortUrl);
-            articleLinkEl.setAttribute("target","_blank");
-            articleImgEl.setAttribute("src", articles.response.results[i].fields.thumbnail);
             articleHeadlineEl.textContent = articles.response.results[i].fields.headline;
+            articleAuthorEl.textContent = articles.response.results[i].fields.byline;
+            articleImgEl.setAttribute("src", articles.response.results[i].fields.thumbnail);
+            articleSummaryEl.textContent = articles.response.results[i].fields.trailText;
+            articleLinkEl.setAttribute("href", articles.response.results[i].fields.shortUrl);
+            articleLinkEl.setAttribute("target", "_blank");
+            articleLinkEl.innerText = "CLICK TO READ";
+
+            // set attributes of DOM Elements
+            articleContainerEl.classList.add("news-card");
+            articleLabelEl.classList.add("article-label");
 
             // combine DOM Elements and add  to container
-            articleLinkEl.append(articleImgEl);
-            articleEl.appendChild(articleLinkEl);
-            articleEl.appendChild(articleHeadlineEl);
-            newsContainerEl.appendChild(articleEl);
-
+            articleLabelEl.appendChild(articleHeadlineEl);
+            articleLabelEl.appendChild(articleAuthorEl);
+            articleContainerEl.appendChild(articleLabelEl);
+            articleContainerEl.appendChild(articleImgEl);
+            articleContainerEl.appendChild(articleSummaryEl);
+            articleContainerEl.appendChild(articleLinkEl);
+            newsContainerEl.appendChild(articleContainerEl);
         }
     })
 };
 
 //get info from user search for city
-
 function getCityName() {
     var cityName = document.querySelector("#cityname").value;
     getForecast(cityName);
@@ -52,9 +75,9 @@ function makeRow(cityName) {
     var text = cityName;
     liEl.textContent = text;
     var historyEl = document.querySelector('.history');
-    historyEl.onclick = function(event){
+    historyEl.onclick = function (event) {
         console.log(event.target.tagName)
-        if (event.target.tagName === "LI"){
+        if (event.target.tagName === "LI") {
             getForecast(event.target.textContent)
         }
     }
@@ -63,8 +86,8 @@ function makeRow(cityName) {
 
 //fetch weather forecast data for that city from open weather API
 
-function getForecast(cityname) {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&appid=ed3ceecb82da99a626e9f6aef02e2dbb&units=metric")
+function getForecast(cityName) {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=ed3ceecb82da99a626e9f6aef02e2dbb&units=metric")
         .then(function (response) {
             return response.json();
         })
@@ -118,28 +141,32 @@ function getForecast(cityname) {
                             xAxes: [{
                                 gridLines: {
                                     display: false
-                                 },
-                            yAxes: [{
-                                gridLines: {
-                                    display: false
-                                 },
-                                ticks: {
-                                    beginAtZero: true,
-                                }
+                                },
+                                yAxes: [{
+                                    gridLines: {
+                                        display: false
+                                    },
+                                    ticks: {
+                                        beginAtZero: true,
+                                    }
+                                }]
                             }]
-                          }]
                         }
                     }
                 });
             })()
-        
+
         })
 }
 
-document.querySelector("#search-button").addEventListener("click", function(e) {
-    e.preventDefault()
+document.querySelector("#search-button").addEventListener("click", function (e) {
+    e.preventDefault();
     getCityName();
-})
+});
 
-getNews();
+document.querySelector("#myBtnContainer").addEventListener("click", function (e) {
+    e.preventDefault();
+    e.target.classList.add("selected-news");    
+    getNewsCategory();
+});
 
