4a875c3da1dbb946bb0b73995699af0419ee07d8
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2020-10-26 14:07:24 -0400

    small bracket change to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -161,7 +161,6 @@ function storeFavouriteCities(cityName) {
             //fetch hourly weather forecast data for that city from open weather API
 function getForecast(cityName) {
     fetch(
-        // 
         ("https://api.weatherbit.io/v2.0/forecast/hourly?city=" + cityName + "&key=27b520f8184c4e47995699bb7910eddd&hours=5")
     ).then(function (response) {
         // check that api response contains valid weather data
